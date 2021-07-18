4e8116944e9da227a700813330575610749f5a1f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-18 23:43:05 +0530

    sovled first problem

---
+++ watermelon.cpp
@@ -0,0 +1,11 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    int main(){
+        int m;
+        cin >> m;
+        if(m <= 2) cout << "NO" << endl;
+        else if(m %2 ==0) cout <<"YES" << endl;
+        else cout << "NO" << endl;
+        return 0;
+    }
