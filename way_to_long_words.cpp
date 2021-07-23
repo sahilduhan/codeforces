400cd855a4f179e00e2e55690788ffda129f9776
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-23 23:50:37 +0530

    git update

--- way_to_long_words.cpp
+++ way_to_long_words.cpp
@@ -1,39 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int count_string(string str)
+int main()
 {
-    int count = 0;
-    for (int i = 0; i < str.length(); i++)
+    int size = 0;
+    cin >> size;
+    string arr[size];
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    for (int i = 0; i < size; i++)
     {
-        count++;
-    }
-    return count;
-}
-void insert_value(string str)
-{
-    int value = count_string(str);
-    for (int i = 0; i < value; i++)
-    {
-        if (i > 4)
+        string str = arr[i];
+        if (str.length() > 10)
         {
-            int ans = value - 2;
-            string value = to_string(ans);
+            string value = to_string(str.length() - 2);
             str.erase(1, str.length() - 2);
             str.insert(1, value);
         }
+        cout << str << endl;
     }
-    cout << str << endl;
-}
-int main()
-{
-    // int count = 0;
-    string str;
-    int num = 0;
-    cin >> num;
-    for (int k = 0; k < num; k++)
-    {
-        cin >> str;
-    }
-    insert_value(str);
     return 0;
 }
