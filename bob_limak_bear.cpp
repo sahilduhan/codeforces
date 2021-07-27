f1239810de9113a606083860068df6c76bcf3c00
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-27 17:27:22 +0530

    solved

--- bob_limak_bear.cpp
+++ bob_limak_bear.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int weight(int a, int b, int count = 1)
-{
-    if (a == b)
-        return 1;
-    while (a < b)
-    {
-        a *= 3;
-        a *= 2;
-        count++;
-    }
-    return count;
-}
+
 int main()
 {
-    int count;
+    int count = 0;
     int a, b;
     cin >> a >> b;
-    cout << weight(a, b);
-    // cout << count;
+    while (a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        count++;
+    }
+    cout << count;
     return 0;
 }
