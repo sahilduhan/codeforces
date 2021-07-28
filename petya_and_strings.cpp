8070ce6533dd2a0c7a357e2618e90d75d255fc75
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-28 22:43:26 +0530

    git updaet

--- petya_and_strings.cpp
+++ petya_and_strings.cpp
@@ -4,6 +4,11 @@ int main()
 {
     string s1, s2;
     cin >> s1 >> s2;
-
+    if (s1.compare(s2) < 0)
+        cout << "-1";
+    else if (s1.compare(s2) > 0)
+        cout << "1";
+    else
+        cout << "0";
     return 0;
 }
