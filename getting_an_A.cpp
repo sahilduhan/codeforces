20e7eb418dbf99fa92679b06af910b6089af8c4a
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-29 23:33:20 +0530

    git update

---
+++ getting_an_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num = 0;
+    cin >> num;
+    vector<int> arr;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> arr[i];
+    }
+    
+
+    return 0;
+}
