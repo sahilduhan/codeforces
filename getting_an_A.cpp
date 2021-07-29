5f921337ee75d71ed651e4cef202d8f55aef8e88
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-30 00:13:57 +0530

    git udpaet

--- getting_an_A.cpp
+++ getting_an_A.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main()
 {
-    int num = 0;
+    int num = 0, count = 0;
+    float sum = 0;
     cin >> num;
-    vector<int> arr;
+    int arr[num];
     for (int i = 0; i < num; i++)
     {
         cin >> arr[i];
     }
-    
-
+    for (int i = 0; i < num; i++)
+    {
+        sum += arr[i];
+    }
+    // cout << final_val(arr, num, sum);
     return 0;
 }
