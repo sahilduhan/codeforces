ac482398763bf12f152baa1de2f0b6ed0dec7c80
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-30 01:06:01 +0530

    gitgit udpaey

--- getting_an_A.cpp
+++ getting_an_A.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 int main()
 {
-    int num = 0, count = 0;
-    float sum = 0;
-    cin >> num;
-    int arr[num];
-    for (int i = 0; i < num; i++)
-    {
-        cin >> arr[i];
+    int n;cin >> n;
+    vector<int> arr(n,0);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+ 
+    auto sum  = accumulate(arr.begin(), arr.end(),0);
+    float avg = (float)sum/n;
+ 
+    sort(arr.begin(), arr.end());
+    int i = 0;
+    while(avg < 4.5){
+        sum += 5 - arr[i++];
+        avg = (float)sum/n;
     }
-    for (int i = 0; i < num; i++)
-    {
-        sum += arr[i];
-    }
-    // cout << final_val(arr, num, sum);
+    cout << (i > 0 ? i : 0) << endl;
     return 0;
 }
