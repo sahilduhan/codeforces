18ae692ecc9ef18b06e99eb8a9aac1d9ba83ee54
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-16 00:36:28 +0530

    git udpate

--- code_forces/anton_danik.cpp
+++ code_forces/anton_danik.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 int main()
 {
-    int num = 0, i = 0, count_1 = 0, count_2 = 0;
+    int num = 0, count_1 = 0, count_2 = 0;
     cin >> num;
-    
+    string str;
+    cin>>str;
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == 'A') count_1++;
+        if(str[i] == 'D') count_2++;
+    }
+    if(count_1 == count_2) cout<<"Friendship"<<endl;
+    if(count_1 > count_2) cout<<"Anton"<<endl;
+    if(count_1 < count_2) cout<<"Danik"<<endl;
+    return 0;
 }
