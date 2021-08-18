7a75c5d7635c9842db475a0bdc1f2a52e1c66c0c
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-18 23:59:40 +0530

    git update

---
+++ code_forces/queue_at_school.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num = 0, i = 0;
+    cin >> num;
+    while (num--)
+    {
+
+        cin >> i;
+    }
+
+    return 0;
+}// question samaj nai aaya
