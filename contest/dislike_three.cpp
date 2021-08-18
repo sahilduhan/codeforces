1b4906516efc0ec8ff1d1ee2c52b51d4bf2f8a9a
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-18 21:48:56 +0530

    git updaet

---
+++ contest/dislike_three.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n; int i = 0;
+    while (n--)
+    {
+        i++;
+        while (1)
+        {
+            if (i % 3 != 0 && i % 10 != 3)
+                break;
+            i++;
+        }
+    }
+    cout <<i<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t; while (t--)
+        solve();
+    return 0;
+}
