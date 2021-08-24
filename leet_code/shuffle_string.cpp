44de6350602e1bf76c5d877a2161b3e229a0cb44
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-25 00:06:34 +0530

    git update

--- leet_code/shuffle_string.cpp
+++ leet_code/shuffle_string.cpp
@@ -6,7 +6,7 @@ public:
     string restoreString(string s, vector<int> &indices)
     {
         string ans;
-        for (int i = 0; i < indices.size(); i++) ans.push_back(s[indices[i]]);
+        for (int i = 0; i < indices.size(); i++) ans[indices[i]] = s[i];
         return ans;
     }
 };
