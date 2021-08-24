91a05194942b9aef957bca9332237628167a83b4
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-25 00:00:54 +0530

    git updtae

--- leet_code/richest_customer.cpp
+++ leet_code/richest_customer.cpp
@@ -5,6 +5,10 @@ class Solution
 public:
     int maximumWealth(vector<vector<int>> &accounts)
     {
+        int max_value = INT_MIN;
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            
+        }
     }
-    
 };
