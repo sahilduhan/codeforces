9de2d397baa9f2b132a7b5782cd6c291e0511c83
Sahil Duhan <duhan.sahil16@gmail.com>
2021-09-25 16:31:32 +0530

    asvca

---
+++ leet_code/binary_tree/second_max.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+        vector<int> ans;
+        int final_val=0;
+        int findSecondMinimumValue(TreeNode* root) 
+        {
+
+
+        return final_val;
+    }
+};
+int main()
+{
+
+    return 0;
+}
