e161429b02e0c1356e6cfeae4465c857dff73415
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-01 23:43:39 +0530

    git udpate

--- leet_code/arrays/count_matching_rule.cpp
+++ leet_code/arrays/count_matching_rule.cpp
@@ -4,14 +4,13 @@ class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
         int final_ans=0;
-        
+
 
         return final_ans;
     }
 };
 int main()
 {
-
-
+    
     return 0;
 }
