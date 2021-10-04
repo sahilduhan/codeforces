7518db2a1124ded3c5fb33825d48fc1531115ccb
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-04 11:15:25 +0530

    nldkf

--- leet_code/arrays/sum_odd_sub_arr.cpp
+++ leet_code/arrays/sum_odd_sub_arr.cpp
@@ -6,7 +6,13 @@ public:
     int sumOddLengthSubarrays(vector<int> &arr)
     {
         int final_ans = 0;
-
+        for (int i = 0; i < arr.size(); i++)
+        {
+            for(int j = i; j < arr.size(); j++)
+            {
+                
+            }
+        }
         return final_ans;
     }
 };
