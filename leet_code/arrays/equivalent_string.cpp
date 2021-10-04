5ae2d8f78ad0c0d3054f7f1f25cf75fc28076c8e
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-04 12:05:52 +0530

    git udapte

---
+++ leet_code/arrays/equivalent_string.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2)
+    {
+        string ans_1 = "", ans_2 = "";
+        for (auto i : word1) ans_1 += i;
+        for(auto i: word2) ans_2 += i;
+        return ans_1 == ans_2;
+    }
+};
+int main()
+{
+
+    return 0;
+}
