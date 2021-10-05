51fa929a14652615ed082b00bca51cb8ed96b826
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-05 11:55:37 +0530

    git dpate

---
+++ leet_code/strings/goal_parser.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string interpret(string command)
+    {
+        string result = "";
+        for (int i = 0; i < command.length(); i++)
+        {
+            
+        }
+        return result;
+    }
+};
+int main()
+{
+
+    return 0;
+}
