40af36cc2e3c07d072dcf1629870d30dc93cd518
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-07 01:00:06 +0530

    git duapet

---
+++ leet_code/binary_search/non_negative_num.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int countNegatives(vector<vector<int>> &grid)
+    {
+        int countNegatives = 0;
+        
+        return countNegatives;
+    }
+};
+int main()
+{
+
+    return 0;
+}
