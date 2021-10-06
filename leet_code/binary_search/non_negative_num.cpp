e304e3a53c138f6d038c28ae693183321f74d5bd
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-07 01:15:22 +0530

    git dyapte

--- leet_code/binary_search/non_negative_num.cpp
+++ leet_code/binary_search/non_negative_num.cpp
@@ -6,7 +6,13 @@ public:
     int countNegatives(vector<vector<int>> &grid)
     {
         int countNegatives = 0;
-        
+        for(int i = 0; i < grid.size(); i++)
+        {
+            for(int j = 0; j < grid[i].size(); j++)
+            {
+                if(grid[i][j]<0) countNegatives++;
+            }
+        }
         return countNegatives;
     }
 };
