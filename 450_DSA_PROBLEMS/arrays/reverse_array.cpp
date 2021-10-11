689819734035e3e164d788d10d284d2b78d6799c
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-11 10:42:27 +0530

    u[date

---
+++ 450_DSA_PROBLEMS/arrays/reverse_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> reverseArray(vector<int> &arr)
+{
+    vector<int> ans;
+    // reverse(arr.begin(), arr.end());
+
+
+    return ans;
+}
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArray(arr);
+    for (auto x : arr)
+        cout << x << endl;
+    return 0;
+}
