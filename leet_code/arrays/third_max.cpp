dbd006ab6858695a0812b70d638bae866b3c7ba9
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-12 00:14:50 +0530

    still trying the problem

--- leet_code/arrays/third_max.cpp
+++ leet_code/arrays/third_max.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 class Solution
 {
 public:
     int thirdMax(vector<int> &nums)
     {
-        int count = 0;
-
-        return count;
+        sort(nums.begin(), nums.end(), greater<>());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        if(nums.size()>=3) return nums[2];
+        return nums[0];
     }
 };
 int main()
