64032768f158fb60b1b6b7a806675216c54a38dd
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-11 10:18:16 +0530

    git dypaer

---
+++ leet_code/binary_search/badVersion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution
+{
+public:
+    int firstBadVersion(int n)
+    {
+        int ans = 0;
+
+        return ans;
+    }
+};
+int main()
+{
+
+    return 0;
+}
