f385d45e195681d53d0d88d3abf012c0bd33fa1f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-19 23:42:24 +0530

    git dpate

--- leet_code/arrays/flower_bed.cpp
+++ leet_code/arrays/flower_bed.cpp
@@ -3,12 +3,9 @@ using namespace std;
 class Solution
 {
 public:
-    bool canPlaceFlowers(vector<int> &flowerbed, int n)
-    {
-        for (int i = 0; i < flowerbed.size(); i++)
-        {
-            if (flowerbed[n] == 0 && flowerbed[n - 1] == 0 || flowerbed[n + 1] == 0)
-                return false;
+    bool canPlaceFlowers(vector<int> &flowerbed, int n){
+        for (int i = 0; i < flowerbed.size(); i++){
+            if (flowerbed[n] == 0 && flowerbed[n - 1] == 0 || flowerbed[n + 1] == 0) return false;
         }
         return true;
     }
