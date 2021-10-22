f5b0a91605849d42918839c06262422b84cd55ac
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-23 01:50:17 +0530

    git udpate

---
+++ leet_code/strings/longPressed.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        if(name==typed) return true;
+        
+        return false;
+    }
+};
+int main()
+{
+
+    return 0;
+}
