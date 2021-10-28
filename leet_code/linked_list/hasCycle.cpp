8aafa88d7c3f506bc2c1e0b2d89375f43476a600
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-28 13:35:47 +0530

    git duaet

---
+++ leet_code/linked_list/hasCycle.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+//  * Definition for singly-linked list.
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *temp;
+        ListNode *itr;
+        while (itr != NULL)
+        {
+        }
+        return false;
+    }
+};
+int main()
+{
+
+    return 0;
+}
