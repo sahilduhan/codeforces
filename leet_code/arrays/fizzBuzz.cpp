23a6dd3df1236a2ebd7f8cab9368777cbf89573b
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-29 23:43:30 +0530

    git udapet

--- leet_code/arrays/fizzBuzz.cpp
+++ leet_code/arrays/fizzBuzz.cpp
@@ -5,14 +5,18 @@ class Solution
 public:
     vector<string> fizzBuzz(int n)
     {
-        vector<string> v;
-        for(int i=0;i<n;i++){
-            if(i%3==0) v.push_back("Fizz");
-            if(i%5==0) v.push_back("Buzz");
-            if(i%3==0 && i%5==0) v.push_back("FizzBuzz");
-            else v.push_back(to_string(i));
+        vector<string> sol;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0) sol.push_back("FizzBuzz");
+            else if (i % 3 == 0) sol.push_back("Fizz");
+            else if (i % 5 == 0) sol.push_back("Buzz");
+            else{
+                string s = to_string(i);
+                sol.push_back(s);
+            }
         }
-        return v;
+        return sol;
     }
 };
 int main()
