79ccc126a02be0a38db56dfa46e618101f7ee572
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-30 14:39:10 +0530

    git udpate

---
+++ leet_code/math/pow(x,n).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double val;
+        val=pow(x,n);
+        return val;
+    }
+};
+int main()
+{
+
+    return 0;
+}
---
+++ leet_code/strings/ReverseInteger.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int ans;
+        return ans;
+    }
+};
+int main()
+{
+
+    return 0;
+}
