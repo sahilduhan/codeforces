24d356daafa029b027cfe57ed6d1194dc091ebd8
Sahil Duhan <duhan.sahil16@gmail.com>
2021-11-01 23:56:03 +0530

    git dupate

--- leet_code/arrays/maxSubarr.cpp
+++ leet_code/arrays/maxSubarr.cpp
@@ -6,15 +6,10 @@ public:
     int maxSubArray(vector<int> &nums)
     {
         int sum = 0, max_sum = INT_MIN;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int current_sum = 0;
-            for (int j = i; j < nums.size(); j++)
-            {
-                current_sum += nums[j];
-                if (current_sum < 0) current_sum = 0;
-                if(current_sum> max_sum) max_sum= current_sum;
-            }
+        for (auto it : nums){
+            sum += it;
+            max_sum= max(sum, max_sum);
+            if(sum<0) sum=0;
         }
         return max_sum;
     }
