acaef056334323663402844b915297f18251e07d
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2021-12-25 00:05:30 +0530

    git ud[ate

---
+++ leet_code/binary_tree/invertTree.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right): val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        TreeNode* temp = NULL;
+        TreeNode* curr = NULL;
+        if (root->left) temp = root->left;
+        if (root->right) curr = root->right;
+        root->right = invertTree(temp);
+        root->left = invertTree(curr);
+        return root;
+    }
+};
---
+++ leet_code/binary_tree/kthLargestELement.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
+int main(){
+
+    return 0;
+}
