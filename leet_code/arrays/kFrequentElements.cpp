62b133eaa7b55e48cb81e1d243649cc075ddd60d
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2021-12-25 14:36:00 +0530

    git update

--- leet_code/arrays/kFrequentElements.cpp
+++ leet_code/arrays/kFrequentElements.cpp
@@ -5,8 +5,6 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> ans;
         unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) m[nums[i]]++;
-
         return ans;
     }
 };
