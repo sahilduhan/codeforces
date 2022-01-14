b871e5f414a0fcbd285ec53e4380082eb7267df3
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2022-01-15 00:56:33 +0530

    git updaet

--- leet_code/math/reverseInt.cpp
+++ leet_code/math/reverseInt.cpp
@@ -3,9 +3,13 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        string str = to_string(x);
-
-
+        long long int ans = 0;
+        while (x != 0){
+            ans = ans * 10 + x % 10;
+            x /= 10;
+            if (ans > INT_MAX or ans < INT_MIN) return 0;
+        }
+        return ans;
     }
 };
 int main(){
