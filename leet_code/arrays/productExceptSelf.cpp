fadbf03e2d93722f582443002542de95ad200826
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2022-02-12 14:22:10 +0530

    git ydpate

--- leet_code/arrays/productExceptSelf.cpp
+++ leet_code/arrays/productExceptSelf.cpp
@@ -3,12 +3,21 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> result;
-        
+        int product = 1;
+        vector<int> output;
+        int n = nums.size();
 
-
-
-        return result;
+        for (auto it : nums){
+            product *= it;
+            output.push_back(product);
+        }
+        product = 1;
+        for (int i = n - 1; i > 0;i--){
+            output[i] = output[i - 1] * product;
+            product *= nums[i];
+        }
+        output[0] = product;
+        return output;
     }
 };
 int main()
