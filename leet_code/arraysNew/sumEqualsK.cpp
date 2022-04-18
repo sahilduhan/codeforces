1032af825c14c9c8f3d0a14e62b8d83feac3f273
Sahil Duhan <74130869+sahilduhan@users.noreply.github.com>
2022-04-19 00:57:36 +0530

    git udapet

---
+++ leet_code/arraysNew/sumEqualsK.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int>mp;
+        int ans = 0;
+        int sum = 0;
+        mp[0] = 1;
+        for (int i = 0;i < nums.size();i++){
+            sum += nums[i];
+            if (mp.find(sum - k) != mp.end()) ans += mp[sum - k];
+            mp[sum] += 1;
+        }
+        return ans;
+    }
+};
+int main(){
+
+
+
+    return 0;
+}
+
+
---
+++ leet_code/sorting/kFrequentWords.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string>ans;
+
+        unordered_map<string, int>mp;
+        for (auto it : words) mp[it]++;
+
+        
+
+
+        return ans;
+    }
+};
+int main(){
+
+    return 0;
+}
